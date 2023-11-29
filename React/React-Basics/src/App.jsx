52fddb63b0b849376f26a8a43f4072e383f08d54
AnandRP2030 <anand.rp2030@gmail.com>
2023-11-29 22:51:45 +0530

    updated file names

--- React/React-Basics/src/App.jsx
+++ React/React-Basics/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import AsyncAwait from "./Components/Async-Await/AsyncAwait";
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<h1> Home Page </h1>} />
-          <Route path="/pure-components" element={<h1> Home Page </h1>} />
+          {/* <Route path="/pure-components" element={<h1> Home Page </h1>} /> */}
+          <Route path="/async" element={<AsyncAwait />} />
         </Routes>
       </BrowserRouter>
     </>
